0f0f7232fbedec8a4bf4578e006992d1d60de976
Janek <development@superyu.xyz>
2023-12-30 18:57:49 +0100

    Fix for docs

--- build-docs.sh
+++ build-docs.sh
@@ -1,4 +1,4 @@
 cargo doc --no-deps
 rm -rf ./docs
-echo "<meta http-equiv=\"refresh\" content=\"0; url=build_wheel\">" > target/doc/index.html
+echo "<meta http-equiv=\"refresh\" content=\"0; url=csflow/index.html\">" > target/doc/index.html
 cp -r target/doc ./docs
