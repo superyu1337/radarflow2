75b997999cba949bdc098b1ded2ac7c1a8737aa7
Janek <development@superyu.xyz>
2024-04-12 14:41:52 +0200

    commit frontend rewrite initial project

---
+++ webradar/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Radar from './components/Radar.vue'
+</script>
+
+<template>
+  <Radar/>
+</template>
+
+<style>
+    :root {
+      background-color: black;
+    }
+</style>
